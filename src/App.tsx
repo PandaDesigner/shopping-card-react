8be7fd3c83c159ad41b209f459e05233b3f92f4e
Pedro Fernandez <pedro@pandadesigners.com>
2025-05-05 09:53:07 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import {Button} from "@/components/ui/button.tsx";
+
+function App () {
+
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center min-h-svh">
+                <Button variant="default">Click me</Button>
+            </div>
+        </>
+    )
+}
+
+export default App
